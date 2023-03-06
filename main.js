863839521585b402ec07a2e3c0b0f0b5d2ef25e3
FranLobo <floboflores@gmail.com>
2023-03-06 18:40:49 +0100

    A bit changes in main.js

--- main.js
+++ main.js
@@ -1,42 +1,48 @@
 //Variable´s starting
 let uncoverCard = 0;
-let card1 = null;
-let card2 = null;
-firstResult = null;
-secondResult = null;
-let movements = 0;
-let success = 0;
 let timing = false;
-let timer = 30;
+let same = 0;
 let initialTimer = 30;
-let resetTimer = null;
+let timer = 30;
+let success = 0;
+let movements = 0;
 
 // Pointing to document
 let showmovements = document.getElementById("movements");
 let showSucces = document.getElementById("success");
 let showTime = document.getElementById("time_left");
+
+let winAudio = new Audio("./sound/win.wav");
+let loseAudio = new Audio("/sound/lose.wav");
+let clickAudio = new Audio("/sound/click.wav");
+
 //Generate random numbers
 let numbers = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8];
-numbers = numbers.sort(() => {
-  return Math.random() - 0.5;
+numbers = numbers.sort(function () {
+  return Math.random() - 0.3;
 });
 console.log(numbers);
 
 function counterTime() {
-  resetTimer = setInterval(() => {
-    timer--;
-    showTime.innerHTML = `Tiempo: ${timer} segundinos`;
-    if (timer == 0) {
-      clearInterval(resetTimer);
-      blockCard();
-    }
-  }, 1000);
+  resetTimer = setInterval(
+    () => {
+      showTime.innerHTML = `Tiempo: ${timer} segundinos`;
+      timer--;
+      if (timer < 0) {
+        clearInterval(resetTimer);
+        blockCard(numbers);
+        loseAudio.play(); //insert audio
+      }
+    },
+    1000,
+    timer
+  );
 }
 
-function blockCard() {
+function blockCard(numbers) {
   for (let i = 0; i <= 15; i++) {
-    let cardBlock = document.getElementById[i];
-    cardBlock.innerHTML = numbers[i];
+    let cardBlock = document.getElementById(i);
+    cardBlock.innerHTML = `<image src="./images/${numbers[i]}.png" alt="">`;
     cardBlock.disabled = true;
   }
 }
@@ -46,55 +52,60 @@ function uncover(id) {
     counterTime();
     timing = true;
   }
-  uncoverCard++;
 
-  console.log(uncoverCard);
-
-  if (uncoverCard == 1) {
-    card1 = document.getElementById(id);
+  if (uncoverCard == 0) {
+    let card1 = document.getElementById(id);
     firstResult = numbers[id];
-    card1.innerHTML = firstResult;
-
+    card1.innerHTML = `<img src="./images/${firstResult}.png" alt="" >`;
+    clickAudio.play();
     // Disable first button
     card1.disabled = true;
+    uncoverCard++;
+
+    firstId = id;
     //Show second button
-  } else if (uncoverCard == 2) {
-    card2 = document.getElementById(id);
+  } else if (uncoverCard == 1) {
+    let card2 = document.getElementById(id);
     secondResult = numbers[id];
-    card2.innerHTML = secondResult;
+    card2.innerHTML = `<img src="./images/${secondResult}.png">`;
 
     // Disable second button
     card2.disabled = true;
+    uncoverCard++;
 
+    secondId = id;
     // Increase movements
     movements++;
     showmovements.innerHTML = `Movimientos: ${movements}`;
+
+    if (firstResult == secondResult) {
+      // Reset of UncoverCard
+      uncoverCard = 0;
+      same++;
+      // Increase Check
+      success++;
+      showSucces.innerHTML = `Aciertos: ${success}`;
+    } else {
+      // Show values momentarily cover again
+      setTimeout(() => {
+        card1 = document.getElementById(firstId);
+        card2 = document.getElementById(secondId);
+        card1.innerHTML = " ";
+        card2.innerHTML = " ";
+        card1.disabled = false;
+        card2.disabled = false;
+        uncoverCard = 0;
+      }, 500);
+    }
   }
 
-  if (firstResult == secondResult) {
-    // Reset of UncoverCard
-    uncoverCard = 0;
-
-    // Increase Check
-    success++;
-    showSucces.innerHTML = `Aciertos: ${success}`;
-
-    if (success == 8) {
-      clearInterval(resetTimer);
-      showSucces.innerHTML = `Aciertos: ${success}🥳`;
-      showTime.innerHTML = `Fantástico! 🎊 solo fueron ${
-        initialTimer - timer
-      } segundinos `;
-      showmovements.innerHTML = `Movimientos: ${movements}😎`;
-    }
-  } else {
-    // Show values momentarily cover again
-    setTimeout(() => {
-      card1.innerHTML = " ";
-      card2.innerHTML = " ";
-      card1.disabled = false;
-      card2.disabled = false;
-      uncoverCard = 0;
-    }, 800);
+  if (same == 8) {
+    winAudio.play();
+    clearInterval(resetTimer);
+    showSucces.innerHTML = `Aciertos: ${success}🥳`;
+    showTime.innerHTML = `Fantástico! 🎊 solo fueron ${
+      initialTimer - timer
+    } segundinos `;
+    showmovements.innerHTML = `Movimientos: ${movements}😎`;
   }
 }
